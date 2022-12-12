5a6c2d6217c353f43aa1d66ad3adcb6feb554209
Rohit Katiyar <rohit.katiyar567@gmail.com>
2022-12-12 13:02:14 +0530

    cleanup activity

--- README.md
+++
@@ -1,2 +0,0 @@
-## Quick start
- Run `npm start` to see the app at `http://localhost:3000`._
--- app/containers/Form/tests/selectors.test.js
+++
@@ -1,7 +0,0 @@
-// import { selectFormDomain } from '../selectors';
-
-describe('selectFormDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
